4c535cf1828dfc38f13b76bf2a6c5e7582174c34
jniero2 <43326093+jniero2@users.noreply.github.com>
2018-10-21 23:02:34 -0500

    Initial Code

--- Lab6/main.c
+++ Lab6/main.c
@@ -12,41 +12,114 @@
 #include "launchpad.h"
 #include "seg7.h"
 #include "seg7digit.h"
+#include "rotary.h"
+#include <driverlib/adc.c>
+#include <driverlib/adc.h>
+#include <math.h>
+
+int degrees;
+
+// The running state of the stopwatch system
+enum {Percentile, Raw}  sysState = Percentile;
+
+// The initial state of the 7-segment display: "00:00" with colon on
+ seg7Display_t seg7Display = {0, 0, 0, 0, 0};
 
 
-//
-// Enable the first sample sequencer to capture the value of channel 0 when
-// the processor trigger occurs.
-//
-ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
-ADCSequenceStepConfigure(ADC0_BASE, 0, 0,
-ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
-ADCSequenceEnable(ADC0_BASE, 0);
 
 
+void
+modedisplay(uint32_t time)
+{
+    if (sysState == Percentile)
+    {
+        degrees = adcVal();
+        degrees = degrees*100/4095;
 
-SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); // Enable the ADC0 module.
+        seg7Display.d4 = 10;
+        seg7Display.d1 = degrees % 10;
 
-while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)){ // Wait for the ADC0 module to be ready.
+        if(degrees < 10)  //erases the value of d3
+               seg7Display.d2 =10;
+        else{
+            seg7Display.d2 = (degrees/10) %10;
+        }
+        if (degrees< 100)
+            seg7Display.d3 =10;
+        else {
+            seg7Display.d3 = (degrees/100) %10;
+        }
 
 }
+    else if (sysState == Raw){
+
+        degrees = adcVal();
 
 
+        seg7Display.d1 = degrees % 10;
+
+        if(degrees < 10)  //erases the value of d3
+               seg7Display.d2 =10;
+        else{
+            seg7Display.d2 = (degrees/10) %10;
+        }
+        if (degrees< 100)
+            seg7Display.d3 =10;
+        else {
+            seg7Display.d3 = (degrees/100) %10;
+        }
+        if (degrees < 1000)
+            seg7Display.d4 = 10;
+        else{
+            seg7Display.d4 = (degrees/1000) %10;
+        }
+    }
+
+    seg7DigitUpdate(&seg7Display);
+    schdCallback(modedisplay, time + 200);
+}
+
+// Callback function for checking push button. SW1 is the RESET button, and
+// SW2 is the START/PAUSE/RESUME button.
+void
+checkPushButton(uint32_t time)
+{
+    int code = pbRead();
+    uint32_t delay;
+
+    switch (code) {
+    case 1:                         // SW1 is the Reset button, only when the stopwatch is paused
+        sysState = Percentile;
+        delay = 250;                // software debouncing
+        break;
+
+    case 2:                         // SW2 is the Start/Pause/Resume button
+
+        sysState = Raw;
+        delay = 250;                // software debouncing
+        break;
+
+    default:
+        delay = 10;
+    }
+
+    schdCallback(checkPushButton, time + delay);
+}
 
 int main(void)
 {
     lpInit();
     seg7Init();
+    adcInit();
 
-    uprintf("%s\n\r", "Lab 3: Stopwatch");
+    uprintf("%d\n\r", adcVal());
+    uprintf("%s\n\r", "Lab 6: ADC");
 
-    // Update the clock display
-    //seg7DigitUpdate(&seg7Display);
 
     // Schedule the first callback events for LED flashing and push button checking.
     // Those trigger callback chains. The time unit is millisecond.
-    //schdCallback(stopWatchUpdate, 1000);
-    //schdCallback(checkPushButton, 1005);
+    schdCallback(checkPushButton, 1000);
+    schdCallback(modedisplay, 1000);
 
     // Run the event scheduler to process callback events
     while (true) {
--- Lab6/rotary.c
+++ Lab6/rotary.c
@@ -17,6 +17,7 @@
 #include <driverlib/systick.h>
 #include "launchpad.h"
 #include "rotary.h"
+#include <driverlib/adc.h>
 
 void adcInit() {
 // this code is pulled from page 46 of peripheral driver library under the BB resource tab
@@ -32,7 +33,8 @@ while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)) {
 // Enable the first sample sequencer to capture the value of channel 0 when the processor trigger occurs.
 //
 ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
-ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
+ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_CH0);
+//ADCSequenceStepConfigure(ADC0_BASE, 0, 1, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH4);
 ADCSequenceEnable(ADC0_BASE, 0);
 }
 
@@ -54,7 +56,7 @@ while(!ADCIntStatus(ADC0_BASE, 0, false))
 // Read the value from the ADC.
 //
 ADCSequenceDataGet(ADC0_BASE, 0, &ui32Value);
-ADCIntClear(ACD0_BASE, 0); // This line might need to go somewhere else
-return (uint16_t) ui32value;
+//ADCIntClear(ACD0_BASE, 0); // This line might need to go somewhere else
+return ui32Value;
 
 }
