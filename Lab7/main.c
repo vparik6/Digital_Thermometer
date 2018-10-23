8fc142a66914b7410b43523b8eeeae90f8ba3a63
jniero2 <43326093+jniero2@users.noreply.github.com>
2018-10-23 12:25:29 -0500

    LED brightness Implemented

--- Lab7/main.c
+++ Lab7/main.c
@@ -41,23 +41,51 @@ void ledPlay(uint32_t time)
 {
     static unsigned int angle = 0; // the degree of angle, used for calculating sine value
     int delay = 5;                    // the callback delay
+   int degrees = 0;
+       degrees = adcVal();
 
     // Calculate PWM parameters for red, blue, and green sub-LEDs using sine function.
     // Use phase shift of 60, 30, and 0 degrees for red, blue, and green
     pwm_t red, blue, green;
-    red.pulseWidth = sine(angle + 60) * led.maxPulseWidth;
-    blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth;
-    green.pulseWidth = sine(angle) * led.maxPulseWidth;
-    red.period = green.period = blue.period = led.pwmPeriod;
 
-   int degrees = 0;
-        degrees = adcVal();
+     if(degrees >= 0 && degrees < 800){
+            led.maxPulseWidth = 40;
+            red.pulseWidth = sine(angle + 60) * led.maxPulseWidth;
+            blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth;
+            green.pulseWidth = sine(angle) * led.maxPulseWidth;
+            red.period = green.period = blue.period = led.pwmPeriod;
+       }
 
-    if(degrees < 800 ){
-        led.maxPulseWidth = led.maxPulseWidth + 1000;
-    }
+     if(degrees > 800 && degrees <= 1600){
+             led.maxPulseWidth = 40;
+             red.pulseWidth = sine(angle + 60) * led.maxPulseWidth + 100;
+             blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth + 100;
+             green.pulseWidth = sine(angle) * led.maxPulseWidth + 100;
+             red.period = green.period = blue.period = led.pwmPeriod;
+       }
 
+     if(degrees > 1600 && degrees <= 2400){
+             led.maxPulseWidth = 40;
+             red.pulseWidth = sine(angle + 60) * led.maxPulseWidth + 200;
+             blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth + 200;
+             green.pulseWidth = sine(angle) * led.maxPulseWidth + 200;
+             red.period = green.period = blue.period = led.pwmPeriod;
+       }
 
+     if(degrees > 2400 && degrees <= 3200){
+              led.maxPulseWidth = 40;
+              red.pulseWidth = sine(angle + 60) * led.maxPulseWidth + 300;
+              blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth + 300;
+              green.pulseWidth = sine(angle) * led.maxPulseWidth + 300;
+              red.period = green.period = blue.period = led.pwmPeriod;
+        }
+     if(degrees > 3200 && degrees <= 4096){
+               led.maxPulseWidth = 40;
+               red.pulseWidth = sine(angle + 60) * led.maxPulseWidth + 400;
+               blue.pulseWidth = sine(angle + 30) * led.maxPulseWidth + 400;
+               green.pulseWidth = sine(angle) * led.maxPulseWidth + 400;
+               red.period = green.period = blue.period = led.pwmPeriod;
+        }
 
     // Set the PWM parameters for LED
     ledPwmSet(red, blue, green);
@@ -70,10 +98,12 @@ void ledPlay(uint32_t time)
 }
 
 void main(void)
+
 {
     lpInit();
     ledPwmInit();
     adcInit();
+    //adcVal();
 
     // Schedule the first callback events for LED flashing and push button checking.
     // Those trigger callback chains. The time unit is millisecond.
@@ -86,11 +116,6 @@ void main(void)
     }
 }
 
-/*
-degress = adcVal();
-if degress < 800{ ....}
-if degrees > 800 > 1600
-*/
 
 
 
